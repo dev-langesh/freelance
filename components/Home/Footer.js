10d57a63550436e26d231540c10c2569c88b6075
dev-langesh <dev.langesh@gmail.com>
2022-09-03 18:18:33 +0530

    Footer update

--- components/Home/Footer.js
+++ components/Home/Footer.js
@@ -5,11 +5,10 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import MailIcon from "@mui/icons-material/Mail";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import { Instagram } from "@mui/icons-material";
 
 export default function Footer() {
   return (
-    <section className="text-white bg-blue-500 p-4 space-y-5 flex flex-col md:flex-row md:justify-around md:items-center">
+    <section className="text-white bg-blue-500 p-4 space-y-5 flex flex-col md:flex-row md:justify-around md:items-start md:space-y-0 md:py-10">
       <div className="space-y-2 md:w-[300px]">
         <h1 className="text-xl font-slab font-bold">TheWorkPlace</h1>
         <p>
@@ -31,6 +30,22 @@ export default function Footer() {
         </Link>
         <Link href="/contact">
           <a>Contact</a>
+        </Link>
+      </div>
+
+      <div className="space-y-2 flex flex-col">
+        <h1 className="text-xl font-slab font-bold">Important Links</h1>
+        <Link href="/home">
+          <a>Terms and Conditions</a>
+        </Link>
+        <Link href="/about">
+          <a>Legal</a>
+        </Link>
+        <Link href="/amenities">
+          <a>Business</a>
+        </Link>
+        <Link href="/contact">
+          <a>Partners</a>
         </Link>
       </div>
 
