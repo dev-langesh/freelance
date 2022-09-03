369b2ceea03eaa90a823ba9ba94d392b1dd73ffd
dev-langesh <dev.langesh@gmail.com>
2022-09-03 17:07:41 +0530

    Qualify online

---
+++ components/Home/QualifyOnline.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ViewInArIcon from "@mui/icons-material/ViewInAr";
+import ClassIcon from "@mui/icons-material/Class";
+import { Button, Divider } from "@mui/material";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+
+export default function QualifyOnline() {
+  return (
+    <div className="p-6 space-y-10 flex flex-col md:flex-row items-center justify-between">
+      <section className="w-full md:w-1/2">
+        <h1 className="text-2xl font-bold font-slab py-6 text-gray-600 ">
+          Qualify Online In Seconds
+        </h1>
+        <p className="text-slate-500 leading-loose text-xl ">
+          All it takes is 15 seconds to know how much we can lend you! From
+          there you can speak to a lending specialist or request a Letter Of
+          Offer!
+        </p>
+
+        <div className="flex py-6 space-x-10">
+          <div>
+            <ViewInArIcon className="text-blue-600 text-4xl" />
+            <h2 className="py-4 text-xl font-slab">Flexible Terms</h2>
+          </div>
+
+          <div>
+            <ClassIcon className="text-blue-600 text-4xl" />
+            <h2 className="py-4 text-xl font-slab">Flexible Terms</h2>
+          </div>
+        </div>
+        <Button variant="outlined">Learn More</Button>
+      </section>
+
+      <section className="relative space-y-10">
+        <img
+          src="https://i0.wp.com/urgentbridgingloans.com.au/wp-content/uploads/2020/07/facilites.jpg?fit=640%2C720&ssl=1"
+          className="rounded-xl"
+        />
+        <div className="p-4 text-white bg-blue-500 rounded shadow-black/30 shadow-lg md:absolute bottom-10 right-56 w-[400px] mx-auto">
+          <BusinessCenterIcon className="rounded-full text-3xl text-white " />
+
+          <h1 className="font-bold text-xl py-4 ">Anything You Need</h1>
+          <p>
+            This is a short description elaborating the service you have
+            mentioned above.​​
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import LandingSection from "../components/Home/LandingSection";
 import FundingNeeds from "../components/Home/FundingNeeds";
 import CardGroup from "../components/Home/CardGroup";
 import Section3 from "../components/Home/Section3";
+import QualifyOnline from "../components/Home/QualifyOnline";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <FundingNeeds />
       <CardGroup />
       <Section3 />
+      <QualifyOnline />
     </div>
   );
 }
