2d5b1b27b05d8cdaea5dc1f13ec5fa01f55f20ee
dev-langesh <dev.langesh@gmail.com>
2022-09-04 15:46:53 +0530

    prequalify bug fix

--- pages/prequalify.js
+++ pages/prequalify.js
@@ -1,19 +1,23 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Prequalify() {
   const router = useRouter();
 
   const { qualified, netbr } = router.query;
 
+  useEffect(() => {
+    console.log(router.query);
+  });
+
   return (
     <section className="w-screen h-screen flex justify-center items-center">
       <div className="bg-gradient-to-tr from-blue-500 to-purple-500 p-10 w-11/12 md:w-[400px] text-white rounded-lg space-y-4 flex flex-col items-center">
         <h1 className="text-center font-slab font-bold text-3xl tracking-wider">
-          {qualified ? "Congrats!" : "Sorry"}
+          {qualified === "true" ? "Congrats!" : "Sorry"}
         </h1>
         <p className="text-xl tracking-wider">
-          {qualified ? "You are Qualified" : "You are not Qualified"}
+          {qualified === "true" ? "You are Qualified" : "You are not Qualified"}
         </p>
         <p className="text-center text-xl font-bold font-slab">
           Netbr : {netbr}
